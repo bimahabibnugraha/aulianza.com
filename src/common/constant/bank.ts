f800827746c01057ccbc542008ef84285f08b02c
bimahabibnugraha <165061378+bimahabibnugraha@users.noreply.github.com>
2025-12-05 10:18:59 +0700

    Update bank.ts

--- src/common/constant/bank.ts
+++ src/common/constant/bank.ts
@@ -2,29 +2,9 @@ import { BankAccountsProps } from "../types/bank";
 
 export const BANK_ACCOUNTS: BankAccountsProps[] = [
   {
-    number: "0431030216",
-    holder: "Ryan Aulia",
-    bank: "Bank BCA",
-    type: "mastercard",
-    logo: "/images/payment/bca.png",
-    color: "#0066ae",
-    className:
-      "bg-[#0066ae]/5 border-[#0066ae] hover:bg-[#0066ae] hover:text-white",
-  },
-  {
-    number: "4140023855",
-    holder: "Ryan Aulia",
-    bank: "Bank Permata",
-    type: "visa",
-    logo: "/images/payment/permata.png",
-    color: "#6e9123",
-    className:
-      "bg-[#6e9123]/5 border-[#6e9123] hover:bg-[#6e9123] hover:text-white",
-  },
-  {
-    number: "1820433105",
-    holder: "Ryan Aulia",
-    bank: "Bank BNI",
+    number: "901020264738",
+    holder: "Bima Habib Nugraha",
+    bank: "SeaBank",
     type: "mastercard",
     logo: "/images/payment/bni.png",
     color: "#e55300",
@@ -32,33 +12,33 @@ export const BANK_ACCOUNTS: BankAccountsProps[] = [
       "bg-[#e55300]/5 border-[#e55300] hover:bg-[#e55300] hover:text-white",
   },
   {
-    number: "003701108940505",
-    holder: "Ryan Aulia",
-    bank: "Bank BRI",
+    number: "089530324308",
+    holder: "Bima Habib Nugraha",
+    bank: "DANA",
     type: "mastercard",
-    logo: "/images/payment/bri.png",
-    color: "#005098",
-    className:
-      "bg-[#005098]/5 border-[#005098] hover:bg-[#005098] hover:text-white",
-  },
-  {
-    number: "7283552997",
-    holder: "Ryan Aulia",
-    bank: "Bank UOB | TMRW",
-    type: "mastercard",
-    logo: "/images/payment/uob.png",
+    logo: "/images/payment/dana.png",
     color: "#005EB8",
     className:
       "bg-[#005EB8]/5 border-[#005EB8] hover:bg-[#005EB8] hover:text-white",
   },
   {
-    number: "90011045921",
-    holder: "Ryan Aulia",
-    bank: "Jenius BTPN",
-    type: "Visa",
-    logo: "/images/payment/jenius.png",
-    color: "#20A4DC",
+    number: "089530324308",
+    holder: "Bima Habib Nugraha",
+    bank: "Gopay",
+    type: "mastercard",
+    logo: "/images/payment/gopay.png",
+    color: "#00b7ffff",
     className:
-      "bg-[#20A4DC]/5 border-[#20A4DC] hover:bg-[#20A4DC] hover:text-white",
+      "bg-[#0066ae]/5 border-[#0066ae] hover:bg-[#0066ae] hover:text-white",
+  },
+  {
+    number: "089530324308",
+    holder: "Bima Habib Nugraha",
+    bank: "ShopeePay",
+    type: "visa",
+    logo: "/images/payment/permata.png",
+    color: "#ff2600ff",
+    className:
+      "bg-[#6e9123]/5 border-[#6e9123] hover:bg-[#6e9123] hover:text-white",
   },
 ];
