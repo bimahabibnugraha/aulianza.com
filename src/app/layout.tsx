1a3e23ab357c2db861e56fbcf2ab53fb367be1cd
bimahabibnugraha <165061378+bimahabibnugraha@users.noreply.github.com>
2025-12-05 10:20:05 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import Footer from "@/common/components/Footer";
 const sora = Sora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ryan Aulia - Software Engineer",
+  title: "Bima Habib Nugraha - Profile",
   description: "My Personal Landing Page",
   icons: "/images/aulianza.webp",
   // openGraph: {
